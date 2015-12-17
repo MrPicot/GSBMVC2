49d04be6849de8a95a001c6c272f93fdfa9b1e00
MrPicot <celian.picot01@gmail.com>
2015-12-17 16:47:41 +0100

    Merge origin/master
    Conflicts:
    	include/class.pdogsb.inc.php
    	styles/styles.css

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -1,11 +1,14 @@
 <?php
+
 include("vues/v_sommaire_comp.php");
 $action = $_REQUEST['action'];
-switch($action){
-        case 'validerFrais':{
-		$lesVisiteurs = $pdo->getLesVisiteursAValider();
-                include("vues/v_validerFrais.php");
-		break;
-	}
+$idVisiteur = $_REQUEST['idVisiteur'];
+switch ($action) {
+    case 'validerFrais': {
+            $lesVisiteurs = $pdo->getLesVisiteursAValider();
+            $lesFichesAValider = $pdo->validerFicheFrais($idVisiteur);
+            include("vues/v_validerFrais.php");
+            break;
+        }
 }
 ?>
--- vues/v_validerFrais.php
+++ vues/v_validerFrais.php
@@ -1,45 +1,44 @@
-      <form class="form-vertical" method="POST"  action="index.php?uc=validerFrais&action=validerFrais">
-      <div class="corpsForm">
-          
-          <fieldset>
-            
-            <table>
-                <td>Choisi</td>
-                <td>Nom</td>
-                <td>Prénom</td>
-                <td>État</td>
-                <td>Date</td>
-			<?php
-                        echo '<tr>';
-				foreach ($lesVisiteurs as $unVisiteur)
-				{
-                                        echo '<td><input type="checkbox" name="Validation" value=""></td>';
-                                        echo '<td>'.$nomPersonne = $unVisiteur['nom'].'</td>';
-					echo '<td>'.$prenomPersonne = $unVisiteur['prenom'].'</td>';
-                                        echo '<td>'.$idEtat = $unVisiteur['idEtat'].'</td>';
-                                        echo '<td>'.$dateModif = $unVisiteur['dateModif'].'</td></tr>';			
-				}
-			?>	
-            </table>
-          </fieldset>
-<?php 
-    if (isset($_REQUEST['erreurs'])&& $_REQUEST['erreurForm']=="FraisForfait") 
-    {    
-        foreach($_REQUEST['erreurs'] as $erreur)
-            {
-             echo '<h3 class="text-danger">'.$erreur.'</h3>';
-            }
-     }
-?>
-      </div>
-      <div class="piedForm">
-      <p>                  
-        <button type="submit" class="btn btn-primary">Valider</button>
-        <button type="reset" class="btn btn-primary">Annuler</button>
-      </p> 
-      </div>
-        
-      </form>
-     
+<form class="form-vertical" method="POST"  action="index.php?uc=validerFrais&action=validerFrais">
+    <div class="corpsForm">
 
-  
+        <fieldset>
+
+            <table>
+                <th>Choisi</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>État</th>
+                <th>ID</th>
+                <th>Date</th>
+                <?php
+                echo '<tr>';
+                foreach ($lesVisiteurs as $unVisiteur) {
+                    echo '<td><input type="checkbox" name="id" value="id"></td>';
+                    echo '<td>' . $nomPersonne = $unVisiteur['nom'] . '</td>';
+                    echo '<td>' . $prenomPersonne = $unVisiteur['prenom'] . '</td>';
+                    echo '<td>' . $idEtat = $unVisiteur['idEtat'] . '</td>';
+                    echo '<td>' . $idVisiteur = $unVisiteur['idVisiteur'] . '</td>';
+                    echo '<input type="hidden" name="idVisiteur" value ="'. $unVisiteur['idVisiteur'].'">';
+                    echo '<td>' . $dateModif = $unVisiteur['dateModif'] . '</td></tr>';
+                }
+                ?>	
+            </table>
+        </fieldset>
+        <?php
+        if (isset($_REQUEST['erreurs']) && $_REQUEST['erreurForm'] == "FraisForfait") {
+            foreach ($_REQUEST['erreurs'] as $erreur) {
+                echo '<h3 class="text-danger">' . $erreur . '</h3>';
+            }
+        }
+        ?>
+    </div>
+    <div class="piedForm">
+        <p>                  
+            <button type="submit" class="btn btn-primary">Valider</button>
+            <button type="reset" class="btn btn-primary">Annuler</button>
+        </p> 
+    </div>
+
+</form>
+
+
